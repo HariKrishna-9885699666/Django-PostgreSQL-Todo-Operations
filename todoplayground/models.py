b1d73c3b7f2ba025194a91f6deecdb4988dd0e43
Hari Krishna Anem <hari-krishna@Haris-MacBook-Pro.local>
2024-05-24 17:11:03 +0530

    updating models.py

--- todoplayground/models.py
+++ todoplayground/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class TodoItem(models.Model):
-    task = models.CharField(max_length=100)  # Text field for the task description
+    task = models.CharField(max_length=150)  # Text field for the task description
     completed = models.BooleanField(default=False)  # Boolean field for completion status
     created_at = models.DateTimeField(auto_now_add=True)  # Automatically set when created
 
