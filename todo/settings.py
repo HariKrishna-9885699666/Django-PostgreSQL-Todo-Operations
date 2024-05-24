89e1f8a86f54a7359db770245cbe453ed89c0454
Hari Krishna Anem <hari-krishna@Haris-MacBook-Pro.local>
2024-05-24 17:09:17 +0530

    updating settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-$=5ge1(b9==sxp#k92abcvto!)sucit6@=tn6i+&neu@8oi9@a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-postgresql-todo-operations.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['django-postgresql-todo-operations.onrender.com', '127.0.0.1', 'localhost']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-postgresql-todo-operations.onrender.com',
+]
 
 
 # Application definition
